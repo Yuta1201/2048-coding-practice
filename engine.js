d8a59e1c93d886e375d31ce8d7cb5957d10f3b96
Yuta Uchijo <kgtnttki.yuta@gmail.com>
2018-11-12 23:38:55 +0800

    removed bug there were several bugs and fixed. Thecontent of bug is that addRandomNumber function fills the cell which already has number.

--- engine.js
+++ engine.js
@@ -7,6 +7,8 @@ var randomCell = {
 var tableObject = document.getElementById("playground");
 var forsearch = new Array;
 
+
+//-----表を作成
 for(var i=0; i<4; i++){
     var tr = document.createElement('tr');
     tr.id = 'r' + i;
@@ -19,21 +21,28 @@ for(var i=0; i<4; i++){
     tableObject.appendChild(tr);
 }
 
+
+
+//-----randomCellのそれぞれの値に代入
 function addRandomNumber(){
-    randomCell.num = (Math.floor( Math.random()*2) + 1)*2;
+    randomCell.num = 2;
+    do{
+        randomCell.location = 'cell' + Math.floor( Math.random()*4 ) + Math.floor( Math.random()*4 );
+        searchEmptyCell();
+        console.log(randomCell.location, forsearch.indexOf(randomCell.location), -1 != forsearch.indexOf(randomCell.location));
+    }while(-1 != forsearch.indexOf(randomCell.location))
+    console.log(randomCell.location + ',' + randomCell.num);
     document.getElementById(randomCell.location).textContent = randomCell.num;
 }
 
 function searchEmptyCell(){
     var searching = document.querySelectorAll('td');
-    searching.forEach(function(e){
-        var eId = e.id;
-        var eContent = document.getElementById(eId).textContent;
-        forsearch[i] = 0==eContent ? 0 : 1;
+    searching.forEach(function(e, i){
+        forsearch[i] = document.getElementById(e.id).textContent != '' ? e.id : '';
+        console.log(forsearch[i]);
     })
 }
 
-for(var i=0; i<4; i++){
+for(var i=0; i<16; i++){
     addRandomNumber();
 }
-searchEmptyCell();
